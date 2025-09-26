2adeb1a79d9df1edee388306306942fbd218e707
Abhishek <pampariabhishek32378@gmail.com>
2025-09-26 21:28:42 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,14 +4,17 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0e088f26edef8a840"
 ZONE_ID="Z01311752B6OQNLRGFQK0"
 DOMAIN_NAME="devops86.fun"
+
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else    
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME"  #devops86.fun
     fi
 
     echo "$instance: $IP"
