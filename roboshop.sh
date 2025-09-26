c637ca451c5e3114c8a675342c57349e654c0c7a
Abhishek <pampariabhishek32378@gmail.com>
2025-09-26 15:12:53 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,20 +5,18 @@ SG_ID="sg-0e088f26edef8a840"
 
 for instance in $@
 do
-    #We need to execute dynamically and store output in variable
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro
-    --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
-    Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids 
+    $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances
+    [0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances
-        [0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].
+        PrivateIpAddress' --output text)
     else    
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances
-        [0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].
+        PublicIpAddress' --output text)
     fi
-
 
     echo "$instance: $IP"
 done
