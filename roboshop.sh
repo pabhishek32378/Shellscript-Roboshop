3da82d79905b890f53a5ba83c3233004a8b2f77d
Abhishek <pampariabhishek32378@gmail.com>
2025-09-26 14:53:24 +0530

    roboshop

---
+++ roboshop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0e088f26edef8a840"
+
+for instance in $@
+do
+    #We need to execute dynamically and store output in variable
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro
+    --security-group-ids sg-0e088f26edef8a840 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
+    Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids i-054c61542e5e885a0 --query 'Reservations[0].Instances
+        [0].PrivateIpAddress' --output text)
+    else    
+        IP=$(aws ec2 describe-instances --instance-ids i-054c61542e5e885a0 --query 'Reservations[0].Instances
+        [0].PublicIpAddress' --output text)
+    fi
+
+
+    echo "$instance: $IP"
+done
