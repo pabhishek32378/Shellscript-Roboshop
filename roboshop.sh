25f62659dc4a701be9f906759f425068b47d6d3f
Abhishek <pampariabhishek32378@gmail.com>
2025-09-26 21:24:23 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0e088f26edef8a840"
-
+ZONE_ID="Z01311752B6OQNLRGFQK0"
+DOMAIN_NAME="devops86.fun"
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
@@ -14,4 +15,23 @@ do
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \  # Creating route53 record via shellscript
+    --hosted-zone-id Z01311752B6OQNLRGFQK0 \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"  #means update
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
