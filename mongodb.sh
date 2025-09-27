54dbbebe9d5d1e12ca9be1e3f63fc16e218ee08b
Abhishek <pampariabhishek32378@gmail.com>
2025-09-27 14:05:13 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -40,3 +40,8 @@ VALIDATE $? "Enable MongoDB"
 systemctl start mongod 
 VALIDATE $? "Start MongoDB"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf # update the listen address
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
