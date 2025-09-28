d170a0de36ac04416cdcec79ec49e0ad7158a59e
Abhishek <pampariabhishek32378@gmail.com>
2025-09-28 13:54:04 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -76,10 +76,13 @@ VALIDATE $? "Copy mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
-
-mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Load catalogue product"
-
+INDEX=$(mongosh mongodb.devops86.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue product"
+else
+    echo -e "catalogue products already loaded .... $Y SKIPPING $N"
+fi
 
 systemctl restart catalogue
 VALIDATE $? "Restarted catalogue"
