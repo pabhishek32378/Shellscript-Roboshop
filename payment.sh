6b68e753b830e6f6864d4b4ecb427e87204d511a
Abhishek <pampariabhishek32378@gmail.com>
2025-09-30 21:43:04 +0530

    shipping

--- payment.sh
+++ payment.sh
@@ -9,9 +9,9 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/Shellscript-Roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws86s.fun
+MONGODB_HOST=mongodb.devops86.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MYSQL_HOST=mysql.daws86s.fun
+MYSQL_HOST=mysql.devops86.fun
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
